0ae5532dd984c55f9705b4511c2aae3d03711b3f
loo <luyia@trip.com>
2023-05-21 14:03:40 +0800

    AzureInterview get input from request

--- src/pages/api/azure/interview.ts
+++ src/pages/api/azure/interview.ts
@@ -3,17 +3,37 @@ import { modelGPT35Turbo, modelChatGPT35Turbo } from './common'
 import { HumanChatMessage, SystemChatMessage } from 'langchain/schema'
 
 export default async function AzureInterview(req: NextApiRequest, res: NextApiResponse<any>) {
-    const systemChatMessage = new SystemChatMessage(
-        'you are an interviewee, the job is web front-end developer. I am interviewer. now you start answer my question.'
-    )
-    const humanChatMessage = new HumanChatMessage(
-        'please explain what is the difference between var, let and const in javascript?'
-    )
-
-    if (req.method === 'GET' || req.method === 'POST') {
-        const modelResult = await modelChatGPT35Turbo.call([systemChatMessage, humanChatMessage], { timeout: 10000 })
-        console.log({ modelResult })
-        console.log(`AzureInterview`, { modelResult })
-        return res.status(200).json({ modelResult })
+    let careerType, question
+    if (req.method === 'POST') {
+        const body = req?.body || {}
+        careerType = body.careerType || careerType
+        question = body.question || question
     }
+    if (req.method === 'GET') {
+        console.log(`req.body`, req.body)
+        console.log(`req.query`, req.query)
+        const query = req?.query || {}
+        careerType = query.c || careerType
+        question = query.q || question
+    }
+
+    if (careerType && question) {
+        try {
+            careerType = decodeURIComponent(careerType)
+            question = decodeURIComponent(question)
+            const systemChatMessage = new SystemChatMessage(
+                `you are an interviewee, the job is ${careerType}. I am interviewer. now you start answer my question.`
+            )
+            const humanChatMessage = new HumanChatMessage(`${question}`)
+            const modelResult = await modelChatGPT35Turbo.call([systemChatMessage, humanChatMessage], {
+                timeout: 10000,
+            })
+            console.log(`AzureInterview`, { modelResult })
+            return res.status(200).json({ modelResult })
+        } catch (e) {
+            console.log(`AzureInterview`, { e })
+            return res.status(500).json({ e })
+        }
+    }
+    return res.status(200).json({ body: req.body, query: req.query })
 }
