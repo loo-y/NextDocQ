e90ae90edad6c7770e6e55a91a9baddd11577da4
loo <luyia@trip.com>
2023-05-23 23:02:44 +0800

    add azure tts demo

---
+++ src/pages/api/azure/speechcheck.ts
@@ -0,0 +1,38 @@
+import MicrosoftSpeechSdk from 'microsoft-cognitiveservices-speech-sdk'
+import _ from 'lodash'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const { azureSpeechKey = '', azureSpeechregion = '' } = process.env || {}
+
+// const speechConfig = MicrosoftSpeechSdk.SpeechConfig.fromSubscription(azureSpeechKey, azureSpeechregion);
+// speechConfig.speechRecognitionLanguage = "zh-CN";
+// https://learn.microsoft.com/en-us/azure/cognitive-services/speech-service/language-support?tabs=stt#speech-to-text
+
+export default async function AzureSpeechCheck(req: NextApiRequest, res: NextApiResponse<any>) {
+    res.setHeader('Content-Type', 'application/json')
+    if (azureSpeechKey === 'paste-your-speech-key-here' || azureSpeechregion === 'paste-your-speech-region-here') {
+        res.status(400).send('auth failed')
+    } else {
+        const params = {
+            method: 'POST',
+            headers: {
+                'Ocp-Apim-Subscription-Key': azureSpeechKey,
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        }
+
+        try {
+            const tokenResponse: any = await fetch(
+                `https://${azureSpeechregion}.api.cognitive.microsoft.com/sts/v1.0/issueToken`,
+                params
+            )
+            res.send({ token: tokenResponse.data, region: azureSpeechregion })
+        } catch (err) {
+            res.status(401).send('There was an error authorizing your speech key.')
+        }
+    }
+}
+
+// https://github.com/Azure-Samples/AzureSpeechReactSample
