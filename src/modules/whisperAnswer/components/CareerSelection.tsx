c53c2e4257f1ad15d265e0bbec6684e47c8da3d6
loo <luyia@trip.com>
2023-05-28 16:36:00 +0800

    update listbox width

--- src/modules/whisperAnswer/components/CareerSelection.tsx
+++ src/modules/whisperAnswer/components/CareerSelection.tsx
@@ -35,7 +35,10 @@ const CareerSelection = () => {
                             <Listbox.Label className="inline text-sm font-medium leading-6 text-gray-900">
                                 职位选择
                             </Listbox.Label>
-                            <div className="relative inline-block ml-2 w-96">
+                            <div
+                                className="relative inline-block ml-2 "
+                                style={{ maxWidth: '24rem', minWidth: '12rem' }}
+                            >
                                 <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
                                     <span className="flex items-center">
                                         <span className="block truncate">{selected?.name}</span>
